989129211a24f9450bd3c2cf6301a07da3f89190
ILya Dymshits <pazam@yandex.ru>
2017-05-30 12:12:30 +0300

    add impoovments code

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-const mainScreen = document.querySelector(`main.central`);
-const introScreen = document.querySelector(`div#intro`);
+const mainScreen = document.querySelector(`central`);
+const introScreen = document.querySelector(`#intro`);
 const template = Array.from(document.querySelectorAll(`template`));
+const ARROW_RIGHT_KEY_CODE = 37;
+const ARROW_LEFT_KEY_CODE = 39;
 let screenIndex = 0;
 
 const addIntroTemplate = () => {
@@ -10,32 +12,32 @@ const addIntroTemplate = () => {
 
 addIntroTemplate();
 
-const showScreen = () => {
+const showScreen = (screen) => {
   mainScreen.innerHTML = ``;
-  mainScreen.appendChild(template[screenIndex].content.cloneNode(true));
+  mainScreen.appendChild(template[screen].content.cloneNode(true));
 };
 
 showScreen();
 const nextScreen = () => {
   if (screenIndex < template.length - 1) {
-    ++screenIndex;
-    showScreen();
+    screenIndex++;
+    showScreen(screenIndex);
   }
 };
 const prevScreen = () => {
   if (screenIndex > 0) {
-    --screenIndex;
-    showScreen();
+    screenIndex--;
+    showScreen(screenIndex);
   }
 };
 
 document.addEventListener(`keydown`, function (event) {
   if (event.altKey) {
     switch (event.keyCode) {
-      case 39:
+      case ARROW_RIGHT_KEY_CODE:
         nextScreen();
         break;
-      case 37:
+      case ARROW_LEFT_KEY_CODE:
         prevScreen();
         break;
     }
